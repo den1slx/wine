e645b53239799c45ef781abd64e4945c8afb4552
den1slx <keycoingold@mail.ru>
2023-05-14 02:03:26 +1000

    Clean up

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 from pandas import read_excel
+from collections import defaultdict
 
 
 def date_check(date):
@@ -39,14 +40,23 @@ def get_unique_values(list_):
     return sorted_list
 
 
-def serialize_wines(path_to_excel_file):
-    excel_wines = read_excel(path_to_excel_file)
-    dictionary_wines = excel_wines.to_dict()
+def get_company_age():
+    year_of_foundation = datetime(year=1920, month=1, day=1)
+    now = datetime.now()
+    year_total_seconds = 365 * 24 * 60 * 60
+    delta = (now - year_of_foundation).total_seconds()
+    company_age = int(delta // year_total_seconds)
+    return company_age
 
+
+def serialize_wines(path_to_excel_file):
+    excel_wines = read_excel(path_to_excel_file, keep_default_na=False, na_values=['nan', 'NA', 'N/A'])
+    dictionary_wines = defaultdict(str)
+    dictionary_wines.update(excel_wines.to_dict())
     category, title, sort, price, image = excel_wines.columns.to_list()
     column_range = len(excel_wines[title])
     categories = (get_unique_values(excel_wines[category].to_list()))
-    wines_categories = {}
+    wines_categories = defaultdict(list)  # ??
     wines = []
 
     for i in range(column_range):
@@ -65,41 +75,20 @@ def serialize_wines(path_to_excel_file):
     return wines_categories
 
 
-year_of_foundation = datetime(year=1920, month=1, day=1)
-now = datetime.now()
-year_total_seconds = 365*24*60*60
-delta = (now - year_of_foundation).total_seconds()
-company_age = int(delta // year_total_seconds)
-title = date_check(company_age)
-
+title = date_check(get_company_age())
 categories_wine = serialize_wines('wine2.xlsx')
-
-excel_wines = read_excel('wine.xlsx')
-dictionary_wines = excel_wines.to_dict()
-column_range = len(excel_wines['Название'])
-wines = []
-
-for i in range(column_range):
-    wine = {
-        'title': dictionary_wines['Название'][i],
-        'sort': dictionary_wines['Сорт'][i],
-        'price': dictionary_wines['Цена'][i],
-        'image': f"images/{dictionary_wines['Картинка'][i]}",
-    }
-    wines.append(wine)
-
 
 
 env = Environment(
-    loader=FileSystemLoader('.'),  # where this html file
-    autoescape=select_autoescape(['html', 'xml'])  # standard html settings
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('template.html')  # load pattern from template.html
+template = env.get_template('template.html')
 
-rendered_page = template.render(  # add value for variables in template.html
+rendered_page = template.render(
     company_age=title,
-    wines=wines,
+    categories_wine=categories_wine,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:  # save to index.html filled pattern
