d1cc9925532f7176f132f9cfecccba8754a1e649
den1slx <keycoingold@mail.ru>
2023-05-17 20:25:12 +1000

    Clean up

--- scripts.py
+++ scripts.py
@@ -1,34 +1,28 @@
 from datetime import datetime
-
-import environs
 from pandas import read_excel
 from collections import defaultdict
-from environs import Env
-from pprint import pprint
-import argparse
 
 
-def get_company_with_us(date):
+def create_company_age_string(date):
     date_string = str(date)
-    checklist_1 = '1'
-    checklist_2 = '2', '3', '4'
-    checklist_3 = '0', '5', '6', '7', '8', '9'
-    checklist_4 = '11', '12', '13', '14'
-    # How better named checklist ?
+    digits_1 = '1'
+    digits_2 = '2', '3', '4'
+    digits_3 = '0', '5', '6', '7', '8', '9'
+    numbers = '11', '12', '13', '14'
 
     if len(date_string) > 1:
         if date_string[-1] == '1' and date_string[-2] != '1':
             year = 'год'
-        elif date_string[-1] in checklist_2 and date_string[-2] != '1':
+        elif date_string[-1] in digits_2 and date_string[-2] != '1':
             year = 'года'
-        elif date_string[-1] in checklist_3 or date_string[-2:] in checklist_4:
+        elif date_string[-1] in digits_3 or date_string[-2:] in numbers:
             year = 'лет'
     else:
-        if date_string in checklist_1:
+        if date_string in digits_1:
             year = 'год'
-        elif date_string in checklist_2:
+        elif date_string in digits_2:
             year = 'года'
-        elif date_string in checklist_3:
+        elif date_string in digits_3:
             year = 'лет'
 
     return f'Уже {date} {year} с вами'
@@ -43,52 +37,20 @@ def get_unique_values(list_):
 
 
 def get_company_age():
-    year_of_foundation = datetime(year=1920, month=1, day=1).year
+    foundation_year = 1920
     now = datetime.now().year
-    delta = (now - year_of_foundation)
-    return delta
+    company_age = now - foundation_year
+    return company_age
 
 
 def get_categories_with_wines(path_to_excel_file, sort_by_key=0):
 
     excel_wines = read_excel(path_to_excel_file, keep_default_na=False, na_values=['nan', 'NA', 'N/A'])
     dictionary_wines = excel_wines.to_dict()
-
     keys = list(dictionary_wines.keys())
-    column_range = len(dictionary_wines[keys[0]])
-    categories = (get_unique_values(excel_wines[keys[sort_by_key]].to_list()))
+    categories = get_unique_values(excel_wines[keys[sort_by_key]].to_list())
+    wines = excel_wines.to_dict(orient='records')
     wines_categories = defaultdict(list)
-    wines = [({key: dictionary_wines[key][i] for key in keys}) for i in range(column_range)]
-
     for category in categories:
         wines_categories.update({category: [wine for wine in wines if wine[keys[sort_by_key]] == category]})
     return wines_categories
-
-
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', '-p', help='path to images', default='example.xlsx')
-    return parser
-
-
-def main():
-    env = Env()
-    company_age = get_company_with_us(get_company_age())
-    try:
-        path = env('PATH_TO_IMAGES')
-    except environs.EnvError:
-        path = None
-    if path:
-        categories_wine = get_categories_with_wines(path)
-    else:
-        parser = create_parser()
-        args = parser.parse_args()
-        path = args.path
-        categories_wine = get_categories_with_wines(path)
-
-    print(company_age)
-    pprint(categories_wine, sort_dicts=False)
-
-
-if __name__ == '__main__':
-    main()
