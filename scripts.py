c6583d7c5e86e8eded916a7bcac0dd46b0b78350
den1slx <keycoingold@mail.ru>
2023-05-18 00:15:39 +1000

    Renamed variables

--- scripts.py
+++ scripts.py
@@ -5,24 +5,24 @@ from collections import defaultdict
 
 def create_company_age_string(date):
     date_string = str(date)
-    digits_1 = '1'
-    digits_2 = '2', '3', '4'
-    digits_3 = '0', '5', '6', '7', '8', '9'
+    digit_one = '1'
+    digits = '2', '3', '4'
+    other_digits = '0', '5', '6', '7', '8', '9'
     numbers = '11', '12', '13', '14'
 
     if len(date_string) > 1:
         if date_string[-1] == '1' and date_string[-2] != '1':
             year = 'год'
-        elif date_string[-1] in digits_2 and date_string[-2] != '1':
+        elif date_string[-1] in digits and date_string[-2] != '1':
             year = 'года'
-        elif date_string[-1] in digits_3 or date_string[-2:] in numbers:
+        elif date_string[-1] in other_digits or date_string[-2:] in numbers:
             year = 'лет'
     else:
-        if date_string in digits_1:
+        if date_string in digit_one:
             year = 'год'
-        elif date_string in digits_2:
+        elif date_string in digits:
             year = 'года'
-        elif date_string in digits_3:
+        elif date_string in other_digits:
             year = 'лет'
 
     return f'Уже {date} {year} с вами'
@@ -45,3 +45,4 @@ def get_categories_with_wines(path_to_excel_file):
 
     return wines_categories
 
+print(get_categories_with_wines('example.xlsx'))
